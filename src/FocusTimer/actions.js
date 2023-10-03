b1b8cf7694dcaa5fe8abc0284efc3bea29bb7f9d
Natan Furletti <natan.furletti@outlook.com>
2023-10-03 01:11:35 -0300

    fodas essa merda

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,6 +1,7 @@
-import state from "./elements.js";
+import state from "./state.js";
 import * as timer from "./timer.js";
 import * as el from "./elements.js";
+
 export function toggleRunning() {
   state.isRunning = document.documentElement.classList.toggle("running");
 
